fbe5b17e627390b78f589976fde9e67df693e6a8
Amirkhan <gymmi94@gmail.com>
2025-02-16 16:21:25 +0300

    v1.0

---
+++ test1.js
@@ -0,0 +1,47 @@
+// console.log(Math.floor(Math.random() * 100))
+// {
+//     let name = "Amir";
+//     let age = 30;
+//     let canCodeJs = false;
+
+//     console.log("Hello, my name is " + name + ", " + "I am " + age + " years old and I can code JavaScript: " + canCodeJs);
+// }
+
+// a = 4;
+// b = 3;
+// a2 = a**2;
+// b2 = b**2;
+// c2 = (a2 + b2) ** 0.5;
+// console.log(c2);
+
+
+// let a = 2;
+// let b = 3;
+// let c = 4;
+
+// b += a;
+// a /= c;
+// c %= b;
+
+// console.log(a, c, b)
+
+
+// a = 130;
+// b = (a * 1.61);
+// console.log('Расстояние 130 миль равно ' + b + ' км');
+
+
+// let a = "Hello";
+// a = prompt("world");
+// console.log(a);
+
+
+console.log(true || false);
+
+// let a = "Hello";
+// a = prompt("world");
+// console.log(a);
+
+// let a = "Hello";
+// a = prompt("world");
+// console.log(a);
